34be85f68e84de8b28bf14b4e5b758c3fcc29d88
leonard-h <leonardhockerts@gmail.com>
2025-01-31 14:34:19 +0000

    chapter 4 / tests ok

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -54,6 +54,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 TEMPLATES = [
     {
@@ -66,12 +67,16 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
 ]
 
 STATICFILES_DIRS = [STATIC_DIR]
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
 
 WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from rango import views
+from django.urls import include, path
+# from rango import views
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index')
-]
+    path('rango/', include('rango.urls'))  # Ensure this is included
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
